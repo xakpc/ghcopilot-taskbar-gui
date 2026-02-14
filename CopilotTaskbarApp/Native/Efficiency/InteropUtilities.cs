c60ca68dc66f69225c4bc96cb916589df9ae8820
Pavel Osadchuk <xakz.pc@gmail.com>
2026-02-14 13:41:41 +0900

    feat: add efficiency mode

---
+++ CopilotTaskbarApp/Native/Efficiency/InteropUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+using Windows.Win32.Foundation;
+
+namespace CopilotTaskbarApp.Native.Efficiency;
+
+internal static class InteropUtilities
+{
+    public static BOOL EnsureNonZero(this BOOL value)
+    {
+        if (value.Value == 0)
+        {
+            Marshal.ThrowExceptionForHR(Marshal.GetHRForLastWin32Error());
+        }
+
+        return value;
+    }
+}
